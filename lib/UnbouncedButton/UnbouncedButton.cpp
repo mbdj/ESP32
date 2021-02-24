ccb077d3c3aac6ef3f7714c0aecc4e65b1ed7891
mehdi <mbdj@free.fr>
2021-02-24 19:58:21 +0100

    V2 avec nouvel algo et choix du front détecté

--- lib/UnbouncedButton/UnbouncedButton.cpp
+++ lib/UnbouncedButton/UnbouncedButton.cpp
@@ -1,50 +1,32 @@
 #include <UnbouncedButton.h>
 
-UnbouncedButton::UnbouncedButton(uint8_t pin, uint8_t mode, unsigned long debounceDelay)
+UnbouncedButton::UnbouncedButton(uint8_t pin, uint8_t mode, unsigned long debounceDelay, Edge edge)
+    : pin{pin}, mode{mode}, debounceDelay{debounceDelay}, edge{edge}
 {
-    switch (mode)
-    {
-    case INPUT || INPUT_PULLDOWN:
-        buttonPreviousState = LOW;
-        break;
-    case INPUT_PULLUP:
-        buttonPreviousState = HIGH;
-        break;
-    default:
-        buttonPreviousState = LOW;
-        break;
-    };
-
-    this->pin = pin;
-    this->mode = mode;
-    this->debounceDelay = debounceDelay;
-    previousTimeButtonRead = 0L;
     pinMode(pin, mode);
 };
 
+
 UnbouncedButton::ButtonState UnbouncedButton::buttonState(void)
 {
-    unsigned long now = millis();
-    int buttonPinRead = digitalRead(pin);
     ButtonState buttonState = ButtonState::UNCHANGED;
 
-    // On ne lit la nouvelle valeur que si le délai de rebond est passé
-    if (now - previousTimeButtonRead > debounceDelay)   // voir https://www.youtube.com/watch?v=aPmJdeJrk5M&list=PLuQznwVAhY2V7Uh0aHOgBvaiqRw9VeCis&index=10
+    // une seconde lecture est effectuée après le délai de rebond
+    int buttonPreviousState = digitalRead(pin);
+    delay(debounceDelay);
+    int buttonPinRead = digitalRead(pin);
+
+    // On détecte le front montant ou descendant selon  PULLUP ou PULLDOWN
+    if (edge == Edge::PRESSED)
     {
-
-        if ((mode == INPUT_PULLDOWN && buttonPinRead == HIGH && buttonPreviousState == LOW)
-         || (mode == INPUT_PULLUP && buttonPinRead == LOW && buttonPreviousState == HIGH))
-        {
+        if ((mode == INPUT_PULLDOWN && buttonPinRead == HIGH && buttonPreviousState == LOW)|| (mode == INPUT_PULLUP && buttonPinRead == LOW && buttonPreviousState == HIGH))
             buttonState = ButtonState::PRESSED;
-        };
-
-        // on met à jour l'état courant pour la prochaine lecture du bouton
-        buttonPreviousState = buttonPinRead;
-        previousTimeButtonRead = now;
-    };
+    }
+    else if ((mode == INPUT_PULLDOWN && buttonPinRead == LOW && buttonPreviousState == HIGH) || (mode == INPUT_PULLUP && buttonPinRead == HIGH && buttonPreviousState == LOW))
+        buttonState = ButtonState::PRESSED;
 
     return buttonState;
 };
 
-bool UnbouncedButton::buttonPressed(void)   { return buttonState() == ButtonState::PRESSED; };
+bool UnbouncedButton::buttonPressed(void) { return buttonState() == ButtonState::PRESSED; };
 bool UnbouncedButton::buttonUnchanged(void) { return buttonState() == ButtonState::UNCHANGED; };
--- lib/UnbouncedButton/UnbouncedButton.h
+++ lib/UnbouncedButton/UnbouncedButton.h
@@ -16,15 +16,20 @@ public:
         UNCHANGED
     };
 
-    UnbouncedButton(uint8_t pin, uint8_t mode = INPUT, unsigned long debounceDelay = 5);
+    enum class Edge
+    {
+        PRESSED,
+        RELEASED
+    };
+
+    UnbouncedButton(uint8_t pin, uint8_t mode = INPUT, unsigned long debounceDelay = 5, Edge edge = Edge::PRESSED);
     ButtonState buttonState(void);
     bool buttonPressed(void);   // Le bouton a été pressé
     bool buttonUnchanged(void); // Le bouton n'a pas été pressé
 
 private:
-    unsigned long previousTimeButtonRead;
-    int buttonPreviousState; // LOW ou HIGH
-    uint8_t pin;    // pin du bouton
-    uint8_t mode;   // INPUT ou PULLUP ou PULLDOWN
-    unsigned long debounceDelay;
+    uint8_t pin;  // pin du bouton
+    uint8_t mode; // INPUT ou PULLUP ou PULLDOWN
+    unsigned long debounceDelay;    // délai mini du rebond après lequel on reteste l'état du bouton
+    Edge edge; // quel changement de front est détecté ?
 };
