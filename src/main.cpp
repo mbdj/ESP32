07fc124d0a5b8598114986df427da5a31ace5c09
mehdi <mbdj@free.fr>
2021-02-24 09:55:56 +0100

    Version avec bouton en PULLDOWN (OK)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 const uint8_t PIN_LED{5};
 const uint8_t PIN_BOUTON{4};
 
-const unsigned long debounceDelay{5};
+const unsigned long debounceDelay{10};
 static unsigned long lastTimeEtatBouton{0L};
-static uint8_t etatPrecedentBouton{HIGH};
-static uint8_t etatBouton{HIGH};
+static uint8_t etatPrecedentBouton{LOW};
+static uint8_t etatBouton{LOW};
 static unsigned long now;
 
 static int nbreClic{0};
@@ -18,7 +18,7 @@ void setup()
 
   // configuration des pin de l'ESP32
   pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_BOUTON, INPUT_PULLUP);
+  pinMode(PIN_BOUTON, INPUT_PULLDOWN);
 
   digitalWrite(PIN_LED, LOW);
 }
@@ -31,7 +31,7 @@ void loop()
   {
     etatBouton = digitalRead(PIN_BOUTON);
 
-    if (etatBouton == LOW && etatPrecedentBouton == HIGH)
+    if (etatBouton == HIGH && etatPrecedentBouton == LOW)
     {
       // on change l'état de la LED à l'appui du bouton
       digitalWrite(PIN_LED, !digitalRead(PIN_LED));
